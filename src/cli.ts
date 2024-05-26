cd3aa30ccf842074a4c8f8c52927c9f0d83206e6
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-05-26 14:26:08 +0530

    finish cli script

--- src/cli.ts
+++ src/cli.ts
@@ -1,56 +1,50 @@
 #!/usr/bin/env node
 
-import * as fs from "fs";
-import * as path from "path";
 import fetch from "node-fetch";
+import fs from "fs";
 
-interface FileData {
+const [framework, componentId] = process.argv.slice(2);
+
+interface ComponentMetadata {
   name: string;
-  content: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: string;
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  };
 }
 
-const args = process.argv.slice(2);
-
-if (args.length === 0 || args.length > 2) {
+if (!framework || !componentId) {
   console.log(
-    "Invalid arguments \nUsage: \n   blackbladeui <framework> <component-id>",
+    "Invalid arguments. Usage: blackbladeui <framework> <component-id>",
   );
   process.exit(1);
 }
 
 const supportedFrameworks = ["react", "angular", "vue"];
 
-if (!supportedFrameworks.includes(args[0])) {
-  console.log("Invalid framework \nSupported frameworks: react, angular, vue");
+if (!supportedFrameworks.includes(framework)) {
+  console.log("Invalid framework. Supported frameworks: react, angular, vue");
   process.exit(1);
 }
 
-console.log("Fetching component for " + args[0]);
+console.log(`Fetching component for ${framework}`);
 
-const fetchComponent = async (framework: string, componentId: string) => {
+const fetchAndSaveComponent = async (metadata: ComponentMetadata[]) => {
   try {
-    const response = await fetch(
-      `https://api.github.com/repos/HaroonSaifi17/BlackBladeUI/src/components/${framework}/${componentId}/contents`,
-      { headers: { Accept: "application/vnd.github.v3.raw+json" } },
-    );
-
-    if (!response.ok) {
-      console.error(`Error fetching component: ${response.statusText}`);
-      process.exit(1);
+    for (const data of metadata) {
+      const componentResponse = await fetch(data.download_url);
+      const component = await componentResponse.text();
+      fs.writeFileSync(`${process.cwd()}/${data.name}`, component);
     }
-
-    const data: FileData[] = (await response.json()) as FileData[];
-    const componentPath = path.join(process.cwd(), framework, componentId);
-
-    if (!fs.existsSync(componentPath)) {
-      fs.mkdirSync(componentPath, { recursive: true });
-    }
-
-    data.forEach((file) => {
-      const filePath = path.join(componentPath, file.name);
-      fs.writeFileSync(filePath, Buffer.from(file.content, "base64"));
-      console.log(`Saved ${filePath}`);
-    });
 
     console.log("Component fetched and saved successfully.");
   } catch (error: any) {
@@ -58,6 +52,31 @@ const fetchComponent = async (framework: string, componentId: string) => {
     process.exit(1);
   }
 };
-let hh;
 
-fetchComponent(args[0], args[1]);
+const fetchComponentDetails = async (
+  framework: string,
+  componentId: string,
+) => {
+  try {
+    const metaResponse = await fetch(
+      `https://api.github.com/repos/HaroonSaifi17/BlackBladeUI/contents/src/components/${framework}/${componentId}`,
+      { headers: { Accept: "application/vnd.github.v3+json" } },
+    );
+
+    if (!metaResponse.ok) {
+      console.error(
+        `Error fetching component metadata: ${metaResponse.statusText}`,
+      );
+      process.exit(1);
+    }
+
+    const metadata: ComponentMetadata[] =
+      (await metaResponse.json()) as ComponentMetadata[];
+    await fetchAndSaveComponent(metadata);
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+fetchComponentDetails(framework, componentId);
--- web/README.md
+++
@@ -1,3 +0,0 @@
-# BlackBladeUI Website
-
-This is the website for BlackBladeUI. It is built using Angular 17.
